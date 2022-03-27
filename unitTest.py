c123363323b51a765c30c9aeb16e9706d1cd044b
Eduard Khusnutdinov <ed.khusnutdinov@gmail.com>
2022-03-27 23:01:48 +0700

    2 files

--- unitTest.py
+++ unitTest.py
@@ -1,45 +1,7 @@
-
-class UpAndDown:
-
-  def __compare(self, a,b,rule):
-    a=len(a)
-    b=len(b)
-    if rule==1:
-      return a<=b
-    return a>=b
-
-
-  def arrange(self,string)->str:
-    if type(string)!=str:
-        return None
-
-
-    words = string.lower().split()
-
-    if len(words)<2:
-      return string
-
-    if len(words[0])>len(words[1]):
-      words[0],words[1]=words[1],words[0]
-
-
-    rule=1
-
-    for i in range(1,len(words)-1):
-      if not self.__compare(words[i+1],words[i],rule):
-        words[i],words[i+1]=words[i+1],words[i]
-
-      rule=(rule+1)%2
-
-    for i in range(1,len(words),2):
-      words[i]=words[i].upper()
-
-    return " ".join(words)
-
-up_down=UpAndDown()
-
+import upAndDown
 import unittest
 
+up_down=UpAndDown()
 
 class TestUpAndDown(unittest.TestCase):
 
